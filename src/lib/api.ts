b5bfe425aecb4f226458ba3756c19696ae9073fb
luminuszz <luminuszz43@gmail.com>
2023-11-27 23:58:01 -0300

    feat: create back-end integration

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { z } from "zod";
 
 export const taskApi = axios.create({
-  baseURL: import.meta.env.API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const createTaskSchema = z.object({
